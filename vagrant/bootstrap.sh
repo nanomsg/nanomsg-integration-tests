97fbf2311ff251b2d0ac24ecf8470eed0a2eda75
Paul Colomiets <paul@colomiets.name>
2013-11-06 19:09:46 +0200

    No need to rebuild everything from scratch on provision

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,8 +2,7 @@
 sudo apt-get install -y cmake make gcc automake libtool
 
 cd /vagrant
-rm -rf vagrant_build 2>&1 || true
-mkdir vagrant_build
+mkdir vagrant_build || true
 cd vagrant_build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr
 make
