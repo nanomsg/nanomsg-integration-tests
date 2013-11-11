893fd65562ca52fd0ec777c4dc771c4f9a36f13f
Paul Colomiets <paul@colomiets.name>
2013-11-11 13:18:27 +0200

    Fix help for checkfactor.py

--- code/checkfactor.py
+++ code/checkfactor.py
@@ -11,13 +11,13 @@ from nanomsg import Socket, REQ
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('-n', '--requests', metavar="NUM",
-        help="Number of requests to issue (default %default)",
+        help="Number of requests to issue (default %(default)d)",
         default=10000, type=int)
     ap.add_argument('-m', '--max-value', metavar="NUM",
-        help="Maximum number that's sent for factorizing (default %default)",
+        help="Maximum number that's sent for factorizing (default %(default)d)",
         default=10**12, type=int)
     ap.add_argument('--min-value', metavar="NUM",
-        help="Maximum number that's sent for factorizing (default %default)",
+        help="Maximum number that's sent for factorizing (default %(default)d)",
         default=10**11, type=int)
     ap.add_argument('--connect', metavar="ADDR", required=True,
         help="Nanomsg address to connect to",
